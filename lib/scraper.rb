4e61df8640c168ec170c906ff2847fb2f5fb79d8
Janan Akkad <jananakkad@hotmail.co.uk>
2022-07-22 17:16:22 +0100

    commit tree 2ef22417bdd1a17566881b0c9436d99d1516f117

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri.HTML5(html)
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
